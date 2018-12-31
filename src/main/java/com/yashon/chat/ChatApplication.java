f4aa66e55c9f18fc299c9d2fdc42781b77cb364e
yanshouqiang <yan_shou_qiang@126.com>
2018-12-31 13:58:45 +0800

    init

---
+++ src/main/java/com/yashon/chat/ChatApplication.java
@@ -0,0 +1,17 @@
+package com.yashon.chat;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @ClassName ChatApplication
+ * @Description TODO
+ * @Author yashon
+ * @Date 2018/12/31 下午1:48
+ * @Version 1.0
+ **/
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.yashon.chat"})
+public class ChatApplication {
+}
